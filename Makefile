3f9a79a5ab2958801ac1452f7ca7011cf8d3ed32
kimhyunkang <kimhyunkang@gmail.com>
2014-05-08 15:53:00 +0900

    Test some YAML 1.1 specs

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
-lib: Makefile src/yaml/type_size.rs $(wildcard src/yaml/*.rs)
+all: build/.lib.dummy
+
+build/.lib.dummy: Makefile src/yaml/type_size.rs $(wildcard src/yaml/*.rs)
 	mkdir -p build
-	rustc src/yaml/lib.rs -g --out-dir=build
+	rustc src/yaml/lib.rs -g --out-dir=build && touch build/.lib.dummy
 
 build/test: Makefile src/yaml/type_size.rs $(wildcard src/yaml/*.rs)
 	mkdir -p build
 	rustc --test src/yaml/lib.rs -o $@
 
-check: build/test
-	./$<
+build/yaml_spec_test: build/.lib.dummy test/yaml_spec_test.rs
+	mkdir -p build
+	rustc --test test/yaml_spec_test.rs -L build -o build/yaml_spec_test
+
+check: build/test build/yaml_spec_test $(wildcard test/source/*.yml)
+	./build/test && ./build/yaml_spec_test
 
 build/codegen: src/codegen/type_size.c
 	mkdir -p build
---
+++ test/yaml_spec_test.rs
@@ -0,0 +1,163 @@
+#![feature(macro_rules)]
+
+extern crate yaml;
+
+use yaml::constructor::{YamlStandardData, YamlMapping, YamlSequence, YamlString, YamlInteger, YamlFloat};
+use std::os;
+use std::io::{File, BufferedReader};
+
+fn match_file(filename: &str, expected: YamlStandardData) {
+    let file_path = Path::new(os::args()[0]).join("../../test/source").join(filename);
+    println!("{}", file_path.display())
+    let reader = ~BufferedReader::new(File::open(&file_path));
+    match yaml::parse_io(reader) {
+        Ok(mut docs) => if docs.len() == 1 {
+            assert_eq!(docs.shift().unwrap(), expected)
+        } else {
+            fail!("too many number of documents: {:?}", docs)
+        },
+        Err(e) => fail!("parse failure: {:?}", e)
+    }
+}
+
+macro_rules! ystr(
+    ($e: expr) => (
+        YamlString($e.to_owned())
+    )
+)
+
+macro_rules! yint(
+    ($e: expr) => (
+        YamlInteger($e)
+    )
+)
+
+macro_rules! yfloat(
+    ($e: expr) => (
+        YamlFloat($e)
+    )
+)
+
+macro_rules! yseq(
+    ($($e:expr),*) => (
+        YamlSequence(~[$(($e),)*])
+    )
+)
+
+macro_rules! ymap(
+    ($($k:expr : $v:expr),*) => (
+        YamlMapping(~[$((ystr!($k), $v),)*])
+    )
+)
+
+macro_rules! y_cmp_map(
+    ($($k:expr : $v:expr),*) => (
+        YamlMapping(~[$(($k, $v),)*])
+    )
+)
+
+#[test]
+fn sequence_of_scalars() {
+    match_file("ball_players.yml", yseq![ystr!("Mark McGwire"), ystr!("Sammy Sosa"), ystr!("Ken Griffey")]);
+}
+
+#[test]
+fn scalar_mappings() {
+    match_file("player_stat.yml", ymap!{
+                                    "hr": yint!(65),
+                                    "avg": yfloat!(0.278),
+                                    "rbi": yint!(147)
+                                })
+}
+
+#[test]
+fn maps_of_sequences() {
+    match_file("ball_clubs.yml", ymap!{
+                                    "american": yseq![ystr!("Boston Red Sox"), ystr!("Detroit Tigers"), ystr!("New York Yankees")],
+                                    "national": yseq![ystr!("New York Mets"), ystr!("Chicago Cubs"), ystr!("Atlanta Braves")]
+                                })
+}
+
+#[test]
+fn sequence_of_maps() {
+    match_file("multiple_player_stat.yml",
+    yseq![
+        ymap!{
+            "name": ystr!("Mark McGwire"),
+            "hr": yint!(65)
+        },
+        ymap!{
+            "name": ystr!("Sammy Sosa"),
+            "hr": yint!(63)
+        }
+    ])
+}
+
+#[test]
+fn sequence_of_sequences() {
+    match_file("csv.yml",
+    yseq![
+        yseq![ystr!("name"), ystr!("hr")],
+        yseq![ystr!("Mark McGwire"), yint!(65)],
+        yseq![ystr!("Sammy Sosa"), yint!(63)]
+    ])
+}
+
+#[test]
+fn mapping_of_mappings() {
+    match_file("map_map.yml",
+    ymap!{
+        "Mark McGwire": ymap!{ "hr": yint!(65) },
+        "Sammy Sosa": ymap!{ "hr": yint!(63) }
+    })
+}
+
+#[test]
+fn alias() {
+    match_file("alias.yml",
+    ymap!{
+        "hr": yseq![ystr!("Mark McGwire"), ystr!("Sammy Sosa")],
+        "rbi": yseq![ystr!("Sammy Sosa"), ystr!("Ken Griffey")]
+    })
+}
+
+#[test]
+fn complex_keys() {
+    match_file("complex_key.yml",
+    y_cmp_map!{
+        yseq![ystr!("Detroit Tigers"), ystr!("Chicago Cubs")]: yseq![ystr!("2001-07-23")],
+        yseq![ystr!("New York Yankees"), ystr!("Atlanta Braves")]: yseq![ystr!("2001-07-02"), ystr!("2001-08-12"), ystr!("2001-08-14")]
+    })
+}
+
+#[test]
+fn block_literal() {
+    match_file("block_literal.yml", ystr!("\\//||\\/||\n// ||  ||__\n"))
+}
+
+#[test]
+fn plain_scalar() {
+    match_file("plain_scalar.yml", ystr!("Mark McGwire's year was crippled by a knee injury."))
+}
+
+#[test]
+fn quoted_scalar() {
+    match_file("quoted_scalar.yml",
+    ymap!{
+        "unicode": ystr!("Sosa did fine.\u263A"),
+        "control": ystr!("\x081998\t1999\t2000\n"),
+        "hexesc":  ystr!("\x13\x10 is \r\n"),
+        "single": ystr!(r#""Howdy!" he cried."#),
+        "quoted": ystr!(" # not a 'comment'."),
+        "tie-fighter": ystr!(r"|\-*-/|")
+    })
+}
+
+#[test]
+fn multi_line_scalar() {
+    match_file("multi_line_scalar.yml",
+    ymap!{
+        "plain": ystr!("This unquoted scalar spans many lines."),
+        "quoted": ystr!("So does this quoted scalar.\n")
+    })
+}
