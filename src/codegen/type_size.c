c7d971f64206d357ed4814cac87db336c9c3ffab
kimhyunkang <kimhyunkang@gmail.com>
2014-05-12 19:23:44 +0900

    Remove unnecessary codegen

--- src/codegen/type_size.c
+++ src/codegen/type_size.c
@@ -27,11 +27,6 @@ int main()
     yaml_parser_t dummy_parser;
 
     printf("#[allow(non_camel_case_types)]\n");
-    // FIXME: might not work on big endian machines
-    printf("pub type yaml_error_type_t = u%lu;\n\n", ((size_t)(&dummy_parser.problem) - (size_t)(&dummy_parser)) * 8);
-
-    printf("#[allow(non_camel_case_types)]\n");
-    // FIXME: might not work on big endian machines
     printf("pub type yaml_parser_input_t = [c_int, ..%lu];\n\n", sizeof(dummy_parser.input) / sizeof(int));
     printf("pub fn new_yaml_parser_input_t() -> yaml_parser_input_t {\n");
     printf("    [0, ..%lu]\n", sizeof(dummy_parser.input) / sizeof(int));
@@ -40,7 +35,6 @@ int main()
     yaml_emitter_t dummy_emitter;
 
     printf("#[allow(non_camel_case_types)]\n");
-    // FIXME: might not work on big endian machines
     printf("pub type yaml_emitter_output_t = [c_int, ..%lu];\n\n", sizeof(dummy_emitter.output) / sizeof(int));
     printf("pub fn new_yaml_emitter_output_t() -> yaml_emitter_output_t {\n");
     printf("    [0, ..%lu]\n", sizeof(dummy_emitter.output) / sizeof(int));
@@ -49,11 +43,9 @@ int main()
     yaml_node_t dummy_node;
 
     printf("#[allow(non_camel_case_types)]\n");
-    // FIXME: might not work on big endian machines
     printf("pub type yaml_node_type_t = u%lu;\n\n", ((size_t)(&dummy_node.tag) - (size_t)(&dummy_node)) * 8);
 
     printf("#[allow(non_camel_case_types)]\n");
-    // FIXME: might not work on big endian machines
     printf("pub type yaml_node_data_t = [c_int, ..%lu];\n\n", sizeof(dummy_node.data) / sizeof(int));
     printf("pub fn new_yaml_node_data_t() -> yaml_node_data_t {\n");
     printf("    [0, ..%lu]\n", sizeof(dummy_node.data) / sizeof(int));
