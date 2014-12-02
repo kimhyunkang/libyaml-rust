8c58be18d3b7fda612c457b952545f244c42572f
kimhyunkang <kimhyunkang@gmail.com>
2014-12-02 13:35:05 -0800

    Fix build script for packaging

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 use std::io::{Command, File, Truncate, Write};
+use std::os;
 
 fn main()
 {
+    let dir = Path::new(os::getenv("OUT_DIR").unwrap());
+    let out_file = dir.join("codegen");
     Command::new("gcc").arg("src/codegen/type_size.c")
                        .arg("-o")
-                       .arg("target/codegen")
+                       .arg(out_file.as_vec())
                        .status()
                        .unwrap();
-    let code = Command::new("./target/codegen").output().unwrap();
+    let code = Command::new(out_file.as_vec()).output().unwrap();
     if !code.status.success() {
         panic!("{}", String::from_utf8_lossy(code.error.as_slice()));
     }
