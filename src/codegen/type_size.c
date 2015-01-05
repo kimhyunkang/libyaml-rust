8732e44fb268c90054b86439666c5aae4222dc41
kimhyunkang <kimhyunkang@gmail.com>
2015-01-05 18:48:56 +0900

    Language changes: Fixed size array syntax changed

--- src/codegen/type_size.c
+++ src/codegen/type_size.c
@@ -7,17 +7,17 @@ int main()
     printf("use libc::c_int;\n\n");
 
     printf("#[allow(non_camel_case_types)]\n");
-    printf("pub type yaml_parser_mem_t = [c_int, ..%lu];\n", sizeof(yaml_parser_t) / sizeof(int));
+    printf("pub type yaml_parser_mem_t = [c_int; %lu];\n", sizeof(yaml_parser_t) / sizeof(int));
     printf("pub fn new_yaml_parser_mem_t() -> yaml_parser_mem_t {\n");
-    printf("    [0, ..%lu]\n", sizeof(yaml_parser_t) / sizeof(int));
+    printf("    [0; %lu]\n", sizeof(yaml_parser_t) / sizeof(int));
     printf("}\n\n");
 
     yaml_event_t dummy_event;
 
     printf("#[allow(non_camel_case_types)]\n");
-    printf("pub type yaml_event_data_t = [c_int, ..%lu];\n", sizeof(dummy_event.data) / sizeof(int));
+    printf("pub type yaml_event_data_t = [c_int; %lu];\n", sizeof(dummy_event.data) / sizeof(int));
     printf("pub fn new_yaml_event_data_t() -> yaml_event_data_t {\n");
-    printf("    [0, ..%lu]\n", sizeof(dummy_event.data) / sizeof(int));
+    printf("    [0; %lu]\n", sizeof(dummy_event.data) / sizeof(int));
     printf("}\n\n");
 
     printf("#[allow(non_camel_case_types)]\n");
@@ -56,25 +56,25 @@ int main()
     yaml_parser_t dummy_parser;
 
     printf("#[allow(non_camel_case_types)]\n");
-    printf("pub type yaml_parser_input_t = [c_int, ..%lu];\n\n", sizeof(dummy_parser.input) / sizeof(int));
+    printf("pub type yaml_parser_input_t = [c_int; %lu];\n\n", sizeof(dummy_parser.input) / sizeof(int));
     printf("pub fn new_yaml_parser_input_t() -> yaml_parser_input_t {\n");
-    printf("    [0, ..%lu]\n", sizeof(dummy_parser.input) / sizeof(int));
+    printf("    [0; %lu]\n", sizeof(dummy_parser.input) / sizeof(int));
     printf("}\n\n");
 
     yaml_emitter_t dummy_emitter;
 
     printf("#[allow(non_camel_case_types)]\n");
-    printf("pub type yaml_emitter_output_t = [c_int, ..%lu];\n\n", sizeof(dummy_emitter.output) / sizeof(int));
+    printf("pub type yaml_emitter_output_t = [c_int; %lu];\n\n", sizeof(dummy_emitter.output) / sizeof(int));
     printf("pub fn new_yaml_emitter_output_t() -> yaml_emitter_output_t {\n");
-    printf("    [0, ..%lu]\n", sizeof(dummy_emitter.output) / sizeof(int));
+    printf("    [0; %lu]\n", sizeof(dummy_emitter.output) / sizeof(int));
     printf("}\n\n");
 
     yaml_node_t dummy_node;
 
     printf("#[allow(non_camel_case_types)]\n");
-    printf("pub type yaml_node_data_t = [c_int, ..%lu];\n\n", sizeof(dummy_node.data) / sizeof(int));
+    printf("pub type yaml_node_data_t = [c_int; %lu];\n\n", sizeof(dummy_node.data) / sizeof(int));
     printf("pub fn new_yaml_node_data_t() -> yaml_node_data_t {\n");
-    printf("    [0, ..%lu]\n", sizeof(dummy_node.data) / sizeof(int));
+    printf("    [0; %lu]\n", sizeof(dummy_node.data) / sizeof(int));
     printf("}\n\n");
 
     printf("#[cfg(test)]\n");
