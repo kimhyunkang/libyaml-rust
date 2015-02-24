77be148574b6a59f5f81597908225efdb591d68b
kimhyunkang <kimhyunkang@gmail.com>
2015-02-24 14:03:00 -0800

    Update build.rs to the new io

--- build.rs
+++ build.rs
@@ -1,26 +1,32 @@
-#![feature(core)]
+#![feature(fs)]
 #![feature(io)]
+#![feature(process)]
 #![feature(env)]
 #![feature(path)]
 
-use std::old_io::{Command, File, Truncate, Write};
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
 use std::path;
 use std::env;
 
 fn main()
 {
     let dir_var = env::var("OUT_DIR").unwrap();
-    let dir = path::Path::new(&dir_var);
-    let out_file = dir.join("codegen");
+    let dir = path::Path::new(&dir_var).join("codegen");
+    let out_file = dir.to_str().unwrap();
     Command::new("gcc").arg("src/codegen/type_size.c")
                        .arg("-o")
-                       .arg(out_file.to_str().unwrap())
+                       .arg(out_file)
                        .status()
                        .unwrap();
-    let code = Command::new(out_file.to_str().unwrap()).output().unwrap();
-    if !code.status.success() {
-        panic!("{}", String::from_utf8_lossy(code.error.as_slice()));
+    let output = Command::new(out_file).output().unwrap();
+    if !output.status.success() {
+        panic!("{}", String::from_utf8_lossy(output.stderr.as_slice()));
     }
-    let mut f = File::open_mode(&Path::new("src/type_size.rs"), Truncate, Write);
-    f.write_all(code.output.as_slice()).unwrap();
+    let mut f = match File::create("src/type_size.rs") {
+        Ok(f) => f,
+        Err(e) => panic!("{:?}", e)
+    };
+    f.write_all(output.stdout.as_slice()).unwrap();
 }
