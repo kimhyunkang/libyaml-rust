2e3d5b260c52d80ef5f86dc871550d255e78ffb7
Ashley Towns <ashleyis@me.com>
2015-03-15 00:38:11 +1100

    allow passing in libyaml cflags by envvar

--- build.rs
+++ build.rs
@@ -10,9 +10,11 @@ use std::env;
 fn main()
 {
     let dir_var = env::var("OUT_DIR").unwrap();
+    let compile_flags = env::var("LIBYAML_CFLAGS").unwrap_or("".to_string());
     let dir = path::Path::new(&dir_var).join("codegen");
     let out_file = dir.to_str().unwrap();
     Command::new("gcc").arg("src/codegen/type_size.c")
+                       .arg(&compile_flags)
                        .arg("-o")
                        .arg(out_file)
                        .status()
