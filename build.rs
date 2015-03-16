955900262d95a797cee2b7218143e4ad7772dc8e
kimhyunkang <kimhyunkang@gmail.com>
2015-03-16 16:40:28 -0700

    Remove all unwraps from build script

--- build.rs
+++ build.rs
@@ -1,29 +1,45 @@
-#![feature(io)]
-#![feature(path)]
-
 use std::fs::File;
 use std::io::Write;
-use std::process::Command;
+use std::process::{Command, Output};
 use std::path;
 use std::env;
 
+fn run_cmd(name: &str, command: &mut Command) -> Output {
+    let res = command.output();
+    match res {
+        Ok(output) => if !output.status.success() {
+            panic!("{} failed:\n{}", name, String::from_utf8_lossy(&output.stderr[..]))
+        } else {
+            output
+        },
+        Err(err) =>
+            panic!("{} failed:\n{}", name, err)
+    }
+}
+
 fn main()
 {
-    let dir_var = env::var("OUT_DIR").unwrap();
-    let dir = path::Path::new(&dir_var).join("codegen");
-    let out_file = dir.to_str().unwrap();
-    Command::new("gcc").arg("src/codegen/type_size.c")
-                       .arg("-o")
-                       .arg(out_file)
-                       .status()
-                       .unwrap();
-    let output = Command::new(out_file).output().unwrap();
-    if !output.status.success() {
-        panic!("{}", String::from_utf8_lossy(&output.stderr[..]));
-    }
+    let dir_var = match env::var("OUT_DIR") {
+        Ok(dir) => dir,
+        Err(e) => panic!("Could not get env value OUT_DIR: {}", e)
+    };
+
+    let out_file = path::Path::new(&dir_var).join("codegen");
+    let mut gcc_cmd = Command::new("gcc");
+    gcc_cmd.arg("src/codegen/type_size.c").arg("-o").arg(&out_file);
+
+    run_cmd("gcc", &mut gcc_cmd);
+
+    let mut codegen_cmd = Command::new(&out_file);
+    let output = run_cmd("codegen", &mut codegen_cmd);
+
     let mut f = match File::create("src/type_size.rs") {
         Ok(f) => f,
-        Err(e) => panic!("{:?}", e)
+        Err(e) => panic!("Could not open file src/type_size.rs: {}", e)
     };
-    f.write_all(&output.stdout[..]).unwrap();
+
+    match f.write_all(&output.stdout[..]) {
+        Err(e) => panic!("Could not write to src/type_size.rs: {}", e),
+        Ok(_) => ()
+    }
 }
